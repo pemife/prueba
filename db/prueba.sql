b955b237dd33902a5b5b9d5d40b52c281b6e23c1
Pepe Millan <jose.millan@iesdonana.org>
2019-01-17 19:41:04 +0100

    Carga inicial

--- db/prueba.sql
+++ db/prueba.sql
@@ -2,3 +2,11 @@
 -- Archivo de base de datos --
 ------------------------------
 
+/* DROP TABLE IF EXISTS alumno CASCADE;
+
+CREATE TABLE alumno
+(
+    id          BIGSERIAL       PRIMARY KEY
+  , nombre      VARCHAR(50)
+  , apellidos   VARCHAR(50)
+); */
